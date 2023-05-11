9aab17426e65c7706efb3b0b054e04346bdd740c
Матвей <matvei.ryabchitsky@yandex.ru>
2023-05-11 12:22:33 +0300

    версия для потока БИК2101-09

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ import openpyxl
 
 
 
-"""base_path = 'D:\Desktop\Программы Питон\ИТИП ошибки расписания\main'
-in_files = sorted(glob(base_path + '/*.xls*'))
+#base_path = 'D:\Desktop\Программы Питон\ИТИП ошибки расписания\main'
+#in_files = sorted(glob(base_path + '/*.xls*'))
 #print(in_files)
 df_list = []
-for f in in_files:
-    df_list.append(pd.read_excel(f))
-    print(f)
+for i in range(1,10):
+    df_list.append(pd.read_excel(f'D:\Desktop\Программы Питон\ИТИП ошибки расписания\main\BIK210{i}.xlsx'))
+    print(f'D:\Desktop\Программы Питон\ИТИП ошибки расписания\main\BIK210{i}.xlsx')
 for i in range(len(df_list)):
     col_names = [
         'day',
@@ -66,8 +66,8 @@ for i in range(len(df_list)):
 master_df = df_list[0]
 for i in range(1, len(df_list)):
     master_df = pd.concat([master_df, df_list[i]])
-print(master_df)"""
-exel_file = pd.ExcelFile('D:\Desktop\Программы Питон\ИТИП ошибки расписания\main\BIK2102.xlsx')
+#print(master_df)
+"""exel_file = pd.ExcelFile('D:\Desktop\Программы Питон\ИТИП ошибки расписания\main\BIK2102.xlsx')
 df = pd.read_excel(exel_file)
 #print(df)
 col_names = [
@@ -83,7 +83,7 @@ col_names = [
     'l_type_2',
     'l_room_2'
 ]
-df.columns = col_names
+df.columns = col_names 
 #print(df)
 indexes = list(df.columns)
 indexes[:len(col_names)] = col_names
@@ -112,6 +112,60 @@ norm_t_table.drop(
 )
 print(norm_t_table)
 norm_t_table=norm_t_table[(norm_t_table['lector_1'] == 'Егоров Д.А.') | (norm_t_table['lector_2'] == 'Егоров Д.А.')]
+"""
+lector = master_df['lector_1' or 'lector_2'].unique()
+for i in range(1,len(lector)+1):
+    print(f"{i}.  {lector[i-1]}")
+n = 0
+while (n<=0 or n>len(lector)):
+    n = int(input("Введите номер преподавателя из списка:"))
+    if n<0 or n>len(lector): print("Повторите ввод, ошибка")
+lec = lector[n-1]
+print(lec)
+master_df_1 = master_df[master_df['lector_1'] == lec]
+master_df_1 = master_df_1.iloc[:,0:7]
+master_df_2 = master_df[master_df['lector_2'] == lec]
+#master_df_2 = master_df_2.iloc[:,[0,1,2,7,8,9,10,11]]
+final_df = pd.concat([master_df_1, master_df_2])
+#print(master_df_1)
+#print(master_df_2)
+dict = {
+    1: 'понедельник',
+    2: 'вторник',
+    3: 'среда',
+    4: 'четверг',
+    5: 'пятница',
+    6: 'суббота'
+}
+err = 0
+df_err = pd.DataFrame()
 
+for i in range(1,7):
+    df = final_df[final_df['day'] == dict[i]]
+    #print(f'{i}:')
+    #print(df)
+    for j in range(1,5):
+        df_1 = df[df['l_num'] == j]
+        master_df_1 = df_1[df_1['lector_1'] == lec]
+        if master_df_1.shape[0] > 1:
+            df_l1 = master_df_1[master_df_1['l_type_1'] != 'л.']
+            if df_l1.shape[0] > 1:
+                err = err + 1
+                print(f"Найдена ошибка номер {err}: ")
+                print(df_l1)
+                df_err = pd.concat([df_err, df_l1])
+        master_df_2 = df_1[df_1['lector_2'] == lec]
+        if master_df_2.shape[0] > 1:
+            df_l2 = master_df_2[master_df_2['l_type_2'] != 'л.']
+            if df_l2.shape[0] > 1:
+                err = err + 1
+                print(f"Найдена ошибка номер {err}: ")
+                print(df_l2)
+                df_err = pd.concat([df_err, df_l2])
 
-#master_df.to_excel('D:\Desktop\Программы Питон\ИТИП ошибки расписания\output.xlsx')
+if err == 0:
+    print("Ошибок нет")
+    final_df.to_excel('D:\Desktop\Программы Питон\ИТИП ошибки расписания\output.xlsx')
+else:
+    print("Все ошибки выведены в: D:\Desktop\Программы Питон\ИТИП ошибки расписания\output.xlsx")
+    df_err.to_excel('D:\Desktop\Программы Питон\ИТИП ошибки расписания\output.xlsx')
